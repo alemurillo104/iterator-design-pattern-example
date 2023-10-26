030a212eed78a37f8cdc3c3784b4e233e7c85153
Alejandra Murillo <alemurillo104@gmail.com>
2023-10-26 01:38:54 -0400

    adding READ.ME

--- src/main/java/com/gisoft/main/iterator/pattern/example/IteratorPatternExample.java
+++ src/main/java/com/gisoft/main/iterator/pattern/example/IteratorPatternExample.java
@@ -2,16 +2,16 @@ package com.gisoft.main.iterator.pattern.example;
 
 public class IteratorPatternExample {
 
-        public static void main(String[] args) {
-                System.out.println("Grupo 1 - Ejercicio Patron Iterator");
-                var panel = new ShowPanelUsersUsingIteratorPattern();
-                // GoogleUsers
-                panel.showGoogleUsers();
+    public static void main(String[] args) {
+        System.out.println("Grupo 1 - Ejercicio Patron Iterator");
+        var panel = new ShowPanelUsersUsingIteratorPattern();
+        // GoogleUsers
+        panel.showGoogleUsers();
 
-                // FacebookUsers
-                panel.showFacebookUsers();
+        // FacebookUsers
+        panel.showFacebookUsers();
 
-                // CreUsers
-                panel.showCreUsers();
-        }
+        // CreUsers
+        panel.showCreUsers();
+    }
 }
