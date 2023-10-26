ff4fce5f8a4acb0370cc6f2b1b66cf32a630b50b
Alejandra Murillo <alemurillo104@gmail.com>
2023-10-25 21:28:32 -0400

    empty project

---
+++ src/main/java/com/mycompany/ejerciciopatroniterator/EjercicioPatronIterator.java
@@ -0,0 +1,8 @@
+package com.mycompany.ejerciciopatroniterator;
+
+public class EjercicioPatronIterator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
